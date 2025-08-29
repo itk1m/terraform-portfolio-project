0a42a970f6bee6e562c0ef3208e6c9d69528e42b
Isaac <isaactaekim987@gmail.com>
2025-08-29 15:24:14 -0400

    edit the db table name

--- nextjs-blog/terraform-nextjs/state.tf
+++ nextjs-blog/terraform-nextjs/state.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "ik-my-tf-website-state"
     key = "global/s3/terraform.tfstate"
-    dynamodb_table = "my-db-website-table"
+    dynamodb_table = "ik-my-db-website-table"
   }
 }
